e38a1b4c790137c8dc41f6f931a4b9aaca912ab4
zafirshi <office_makeit@163.com>
2023-07-03 10:22:38 +0800

    clean .idea

--- run.sh
+++ run.sh
@@ -56,6 +56,42 @@ CURRENT_DIR=$(cd $(dirname $0); pwd)
 
 # refine run
 ###-> topk30 and matching ratio 0.9
-CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.Aachen_v1_1.pipeline --outputs outputs/aachen_v1.1/CPU_rz1024_topk30_wMR.9_SGV --feature_conf zippypoint_aachen_n4096_r1024 --num_loc 30 --matcher_conf zippypoint-matcher.9 --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.Aachen_v1_1.pipeline --outputs outputs/aachen_v1.1/CPU_rz1024_topk30_wMR.9_SGV --feature_conf zippypoint_aachen_n4096_r1024 --num_loc 30 --matcher_conf zippypoint-matcher.9 --skip_geometric_verification=True
 ###-> topk50 and matching ratio 0.9
-CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.Aachen_v1_1.pipeline --outputs outputs/aachen_v1.1/CPU_rz1024_topk50_wMR.9_SGV --feature_conf zippypoint_aachen_n4096_r1024 --num_loc 50 --matcher_conf zippypoint-matcher.9 --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.Aachen_v1_1.pipeline --outputs outputs/aachen_v1.1/CPU_rz1024_topk50_wMR.9_SGV --feature_conf zippypoint_aachen_n4096_r1024 --num_loc 50 --matcher_conf zippypoint-matcher.9 --skip_geometric_verification=True
+
+# ChunXiRoad Experiment
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk30_wMR_SGV --feature_conf zippypoint_aachen --num_loc 30 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+# add new sift model in CPU_rz640_topk30_wMR_SGV_nDC5
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk30_wMR_SGV_nDC5 --feature_conf zippypoint_aachen --num_loc 30 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+# add new colmap model from hy CPU_rz640_topk30_wMR_SGV_nDC6
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk30_wMR_SGV_nDC6 --feature_conf zippypoint_aachen --num_loc 30 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+### just change topk30 -> topk10
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR_SGV --feature_conf zippypoint_aachen --num_loc 10 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR_SGV_kThs.01_tf --feature_conf zippypoint_aachen.01 --num_loc 10 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+
+# use onnxruntime
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR_SGV_kThs.01_onnx --feature_conf zippypoint_aachen.01_onnx --num_loc 10 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+
+
+# kThd & onnx/tf
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR_SGV --feature_conf zippypoint_aachen --num_loc 10 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR_SGV_kThd.001 --feature_conf zippypoint_aachen.001 --num_loc 10 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR_SGV_kThd.01 --feature_conf zippypoint_aachen.01 --num_loc 10 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR_SGV_onnx --feature_conf zippypoint_aachen_onnx --num_loc 10 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR_SGV_kThd.001_onnx --feature_conf zippypoint_aachen.001_onnx --num_loc 10 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR_SGV_kThd.01_onnx --feature_conf zippypoint_aachen.01_onnx --num_loc 10 --matcher_conf zippypoint-matcher --skip_geometric_verification=True
+
+# matching ratio
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR.9_SGV --feature_conf zippypoint_aachen --num_loc 10 --matcher_conf zippypoint-matcher.9 --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR.9_SGV_kThd.001 --feature_conf zippypoint_aachen.001 --num_loc 10 --matcher_conf zippypoint-matcher.9 --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR.9_SGV_kThd.01 --feature_conf zippypoint_aachen.01 --num_loc 10 --matcher_conf zippypoint-matcher.9 --skip_geometric_verification=True
+
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR.9_SGV_onnx --feature_conf zippypoint_aachen_onnx --num_loc 10 --matcher_conf zippypoint-matcher.9 --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR.9_SGV_kThd.001_onnx --feature_conf zippypoint_aachen.001_onnx --num_loc 10 --matcher_conf zippypoint-matcher.9 --skip_geometric_verification=True
+#CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/CPU_rz640_topk10_wMR.9_SGV_kThd.01_onnx --feature_conf zippypoint_aachen.01_onnx --num_loc 10 --matcher_conf zippypoint-matcher.9 --skip_geometric_verification=True
+
+# hfnet on ChunXiRoad
+CUDA_VISIBLE_DEVICES='' python3 -m hloc.pipelines.ChunXiRoad.pipeline --outputs outputs/ChunXiRoad/hfnet_onnx_wnms --feature_conf hfnet_onnx --num_loc 10 --matcher_conf NN-mutual --skip_geometric_verification=True
+
